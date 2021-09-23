8ba9fb97a4b58d8cdaf2aba793f4be45b46a0aed
Frank David Addae <frankdavid.addae@gmail.com>
2021-09-23 14:20:37 +0000

    check out card

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:ecommerce_ui/components/default_button.dart';
 import 'package:ecommerce_ui/constants.dart';
 import 'package:ecommerce_ui/models/cart.dart';
 import 'package:ecommerce_ui/screens/cart/components/body.dart';
+import 'package:ecommerce_ui/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CartScreen extends StatelessWidget {
   static String routeName = 'cart-screen';
@@ -14,6 +17,7 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: CartScreenBody(),
+      bottomNavigationBar: CheckOutCard(),
     );
   }
 
@@ -34,6 +38,89 @@ class CartScreen extends StatelessWidget {
             style: Theme.of(context).textTheme.caption,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CheckOutCard extends StatelessWidget {
+  const CheckOutCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(30.0),
+        vertical: getProportionateScreenWidth(15.0),
+      ),
+      // height: 174,
+      decoration: BoxDecoration(
+        color: kWhiteColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20.0,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  height: getProportionateScreenWidth(40.0),
+                  width: getProportionateScreenWidth(40.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F6F9),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: SvgPicture.asset('assets/icons/receipt.svg'),
+                ),
+                Spacer(),
+                Text('Add voucher code'),
+                SizedBox(width: 10),
+                Icon(Icons.arrow_forward_ios, size: 12.0),
+              ],
+            ),
+            SizedBox(height: getProportionateScreenWidth(20.0)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: 'Total:\n',
+                    children: [
+                      TextSpan(
+                        text: '\$${337.15}',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: kBlackColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: getProportionateScreenWidth(190.0),
+                  child: DefaultButton(
+                    buttonText: 'Check Out',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
